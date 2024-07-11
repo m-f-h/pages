2b0f7b8644898dc0cac94e85a351b6baf7c454fa
m-f-h <github@dsi972.fr>
2024-07-11 12:13:02 -0400

    Create script.js

---
+++ sidebars/responsive/brave/script.js
@@ -0,0 +1,11 @@
+<script>
+  // Add event listener to the navigation trigger
+document.querySelector('.cd-side-hide').addEventListener('click', function() {
+  // Toggle the sidebar navigation
+  document.querySelector('.cd-side-nav').classList.toggle('active');
+});
+document.querySelector('.cd-nav-trigger').addEventListener('click', function() {
+  // Toggle the sidebar navigation
+  document.querySelector('.cd-side-nav').classList.toggle('active');
+});
+</script>
