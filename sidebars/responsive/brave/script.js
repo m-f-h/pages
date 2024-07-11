ad4508344360cff9876000c808d6937698fdc51d
m-f-h <github@dsi972.fr>
2024-07-11 12:56:41 -0400

    Update script.js class.toggle(active) => visibility hidden/visible

--- sidebars/responsive/brave/script.js
+++ sidebars/responsive/brave/script.js
@@ -1,9 +1,9 @@
   // Add event listener to the navigation trigger
 document.querySelector('.cd-side-hide').addEventListener('click', function() {
   // Toggle the sidebar navigation
-  document.querySelector('.cd-side-nav').classList.toggle('active');
+  document.querySelector('.cd-side-nav').style.visibility = "hidden";
 });
 document.querySelector('.cd-nav-trigger').addEventListener('click', function() {
   // Toggle the sidebar navigation
-  document.querySelector('.cd-side-nav').classList.toggle('active');
+  document.querySelector('.cd-side-nav').style.visibility = "visible";
 });
